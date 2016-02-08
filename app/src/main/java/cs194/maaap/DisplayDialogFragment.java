2f1aa2a5d32f6db151bfcbc33e87bbfbe8a897c9
Kaidi Yan <kaidi@stanford.edu>
2016-02-07 22:57:12 -0800

    upvote/downvote seems to work

--- app/src/main/java/cs194/maaap/DisplayDialogFragment.java
+++ app/src/main/java/cs194/maaap/DisplayDialogFragment.java
@@ -9,18 +9,14 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.util.Log;
 
 public class DisplayDialogFragment extends DialogFragment {
-
-    int upvoteCnt = 0;
-    int downvoteCnt = 0;
-    Bleat bleat;
+    private Bleat bleat;
 
     public DisplayDialogFragment(Bleat bleat) {
         this.bleat=bleat;
     }
-
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -30,17 +26,31 @@ public class DisplayDialogFragment extends DialogFragment {
         message.setText(bleat.getMessage());
         // Watch for button clicks.
         final Button up = (Button)v.findViewById(R.id.up);
+        final Button down = (Button)v.findViewById(R.id.down);
         up.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                upvoteCnt++;
-                up.setText("Upvotes : " + Integer.toString(upvoteCnt));
+                BleatAction bleatAction = new BleatAction(((MapsActivity)getActivity()));
+                UpvoteBleat upvoteBleat = new UpvoteBleat(bleatAction);
+                try {
+                    upvoteBleat.execute(bleat).get();
+                } catch (Exception e) {
+                    Log.d("map", "ggwp");
+                };
+                up.setText("Upvotes : " + Integer.toString(bleat.getUpvotes().size() - 1));
+                down.setText("Downvotes : " + Integer.toString(bleat.getDownvotes().size() - 1));
             }
         });
-        final Button down = (Button)v.findViewById(R.id.down);
         down.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                downvoteCnt++;
-                down.setText("Downvotes : " + Integer.toString(downvoteCnt));
+                BleatAction bleatAction = new BleatAction(((MapsActivity)getActivity()));
+                DownvoteBleat downvoteBleat = new DownvoteBleat(bleatAction);
+                try {
+                    downvoteBleat.execute(bleat).get();
+                } catch (Exception e) {
+                    Log.d("map", "ggwp");
+                }
+                up.setText("Upvotes : " + Integer.toString(bleat.getUpvotes().size() - 1));
+                down.setText("Downvotes : " + Integer.toString(bleat.getDownvotes().size() - 1));
             }
         });
 
