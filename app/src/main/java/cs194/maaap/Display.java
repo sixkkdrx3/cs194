60a4a563587bf8b366f832a546c9ea1d5b7bd3a6
Kaidi Yan <kaidi@stanford.edu>
2016-02-21 23:57:04 -0800

    a simple UI for displaying comments

--- app/src/main/java/cs194/maaap/Display.java
+++ app/src/main/java/cs194/maaap/Display.java
@@ -2,12 +2,22 @@ package cs194.maaap;
 
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Display extends Activity {
 
@@ -58,10 +68,72 @@ public class Display extends Activity {
             }
         });
 
-        /* test for creating comment */
+        /* test for creating comment
         CommentAction commentAction = new CommentAction(this, bleat.getBID());
         SaveComment saveComment = new SaveComment(commentAction);
         saveComment.execute("what the fk is this comment");
-    }
+        */
 
+        LinearLayout scroll = (LinearLayout)findViewById(R.id.comment_layout);
+        LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        final CommentAction commentAction = new CommentAction(this, bleat.getBID());
+        GetComments getComments = new GetComments(commentAction);
+        List<Comment> allComments = null;
+
+        try {
+            allComments = getComments.execute().get();
+        } catch (Exception e) { }
+
+        for (final Comment comment : allComments) {
+            if (!comment.getBID().equals(bleat.getBID())) continue;
+            View view = inflater.inflate(R.layout.comment_fragment, null);
+            scroll.addView(view);
+            TextView commentMessage = (TextView)view.findViewById(R.id.comment_content);
+            commentMessage.setText(comment.getMessage());
+
+            final TextView commentNetVotes = (TextView)view.findViewById(R.id.num);
+            commentNetVotes.setText(Integer.toString(comment.computeNetUpvotes()));
+
+            final ImageView commentUp = (ImageView)view.findViewById(R.id.up);
+            final ImageView commentDown = (ImageView)view.findViewById(R.id.down);
+
+            commentUp.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    UpvoteComment upvoteComment = new UpvoteComment(commentAction);
+                    try {
+                        upvoteComment.execute(comment).get();
+                    } catch (Exception e) {
+                        Log.d("map", "ggwp");
+                    }
+                    ;
+                    int num = comment.computeNetUpvotes();
+                    commentNetVotes.setText(Integer.toString(num));
+
+                }
+            });
+            commentDown.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    DownvoteComment downvoteComment = new DownvoteComment(commentAction);
+                    try {
+                        downvoteComment.execute(comment).get();
+                    } catch (Exception e) {
+                        Log.d("map", "ggwp");
+                    }
+                    int num = comment.computeNetUpvotes();
+                    commentNetVotes.setText(Integer.toString(num));
+                }
+            });
+        }
+
+        final EditText tv = (EditText)findViewById(R.id.new_comment);
+        Button button = (Button)findViewById(R.id.comment_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                final String res = tv.getText().toString();
+                SaveComment saveComment = new SaveComment(commentAction);
+                saveComment.execute(res);
+            }
+        });
+    }
 }
