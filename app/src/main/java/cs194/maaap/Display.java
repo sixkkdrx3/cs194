27ab3a34e511f54631ef57269a1277db7d4d7a2d
Qianying Lin <qlin1@stanford.edu>
2016-02-21 23:22:33 -0800

    Display

--- app/src/main/java/cs194/maaap/Display.java
+++ app/src/main/java/cs194/maaap/Display.java
@@ -5,6 +5,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Display extends Activity {
@@ -16,10 +18,45 @@ public class Display extends Activity {
         setContentView(R.layout.display);
         Log.d("valll", "hiiii");
         Intent i = getIntent();
-        Bleat bleat = (Bleat)i.getSerializableExtra("myBleat");
+        final Bleat bleat = (Bleat)i.getSerializableExtra("myBleat");
         TextView message = (TextView)findViewById(R.id.bleat_content);
         Log.d("valll", bleat.getMessage());
         message.setText(bleat.getMessage());
+
+        final ImageView up = (ImageView)findViewById(R.id.upvote);
+        final ImageView down = (ImageView)findViewById(R.id.downvote);
+        final TextView number = (TextView)findViewById(R.id.number);
+        int num = bleat.getUpvotes().size() - bleat.getDownvotes().size();
+        number.setText(Integer.toString(num));
+
+        up.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                BleatAction bleatAction = new BleatAction(((MapsActivity)maaap));
+                UpvoteBleat upvoteBleat = new UpvoteBleat(bleatAction);
+                try {
+                    upvoteBleat.execute(bleat).get();
+                } catch (Exception e) {
+                    Log.d("map", "ggwp");
+                }
+                ;
+                int num = bleat.computeNetUpvotes();
+                number.setText(Integer.toString(num));
+
+            }
+        });
+        down.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                BleatAction bleatAction = new BleatAction(((MapsActivity) maaap));
+                DownvoteBleat downvoteBleat = new DownvoteBleat(bleatAction);
+                try {
+                    downvoteBleat.execute(bleat).get();
+                } catch (Exception e) {
+                    Log.d("map", "ggwp");
+                }
+                int num = bleat.getUpvotes().size() - bleat.getDownvotes().size();
+                number.setText(Integer.toString(num));
+            }
+        });
     }
 
 }
